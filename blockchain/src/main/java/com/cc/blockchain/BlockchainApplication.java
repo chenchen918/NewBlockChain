d6cc490ff72a8a5c648cc9f91d109e5fc564f12b
chenchen <2470591284@qq.com>
2019-11-13 19:52:14 +0800

    Simple definition interface

--- blockchain/src/main/java/com/cc/blockchain/BlockchainApplication.java
+++ blockchain/src/main/java/com/cc/blockchain/BlockchainApplication.java
@@ -1,9 +1,11 @@
 package com.cc.blockchain;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.cc.blockchain.dao")
 public class BlockchainApplication {
 
     public static void main(String[] args) {
