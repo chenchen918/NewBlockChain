bcaa73fc13c890e6a21b0974cb7848dbadb13082
chenchen <2470591284@qq.com>
2019-11-22 20:55:51 +0800

    Front-end implementation query

--- demo1/viewmodels/Recentblock.js
+++ demo1/viewmodels/Recentblock.js
@@ -3,10 +3,35 @@ var app = new Vue({
     data: {
         ListRecentBlock: [],
         type: [{ id: 1, name: "块" }, { id: 2, name: "交易" }],
-        blockhash: ""
+        keyword: ""
 
     },
     methods: {
+        handleSearchClick() {
+            console.log('search click');
+
+            if (!isNaN(this.keyword)) {
+                console.log('最近块信息');
+                this.getListRecentBlock();
+                return;
+            }
+
+            if (this.keyword.length < 64) {
+                console.log('跳转地址页面');
+                location.href = 'GetByaddress?address=' + this.keyword;
+                return;
+            }
+
+            if (this.keyword.startsWith('00000')) {
+                console.log('根据hash查询块信息');
+                location.href = 'BlockShow?blockhash=' + this.keyword;
+                return;
+            } else {
+                console.log('交易信息');
+                location.href = 'Transaction?txid=' + this.keyword;
+                return;
+            }
+        },
         ToTransition() {
 
             window.location.href = "http:/Transaction";
